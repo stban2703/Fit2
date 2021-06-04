a66abba8f52fbd85f91e0f1ab7cd5e710fadda7b
stban2703 <s.t.ban1401@gmail.com>
2021-06-03 19:41:49 -0500

    Render activity coincidence

--- src/scripts/result.js
+++ src/scripts/result.js
@@ -4,6 +4,8 @@ const resultListActivitiesContainer = document.querySelector(".resultList--activ
 const resultListAverageContainer = document.querySelector(".resultList--average");
 const resultListUserPreferenceContainer = document.querySelector(".resultList--userPreferences");
 const resultListUserScoreContainer = document.querySelector(".resultList--userScore");
+const coincidenceValueQuantitySpan = document.querySelector(".coincidence__value--quantity");
+const coincidenceValueAverageSpan = document.querySelector(".coincidence__value--average");
 
 let myStorage = window.localStorage;
 let user = JSON.parse(myStorage.getItem("user"));
@@ -16,16 +18,16 @@ renderListResult(friendsSimilarityList, "cosineSimilarity", resultListSimilarity
 renderListResult(recommendedActivities, "Nombre", resultListActivitiesContainer);
 renderListResult(recommendedActivities, "Promedio", resultListAverageContainer);
 renderListResult(userFavoriteActivities, "Nombre", resultListUserPreferenceContainer);
-renderListResult(userFavoriteActivities, "Promedio", resultListUserScoreContainer)
-
+renderListResult(userFavoriteActivities, "Promedio", resultListUserScoreContainer);
+renderCoincidences();
 
 function renderListResult(list, key, htmmlParent) {
     list.forEach((elem) => {
         const liElem = document.createElement("li");
         let text = "";
-        if(key == "cosineSimilarity") {
+        if (key == "cosineSimilarity") {
             text = Math.round(elem[key] * 100) + "%";
-        } else if(key == "Nombre") {
+        } else if (key == "Nombre") {
             text = elem[key].replace(/_/g, ' ');
         } else {
             text = elem[key].toFixed(2);
@@ -35,4 +37,27 @@ function renderListResult(list, key, htmmlParent) {
         `
         htmmlParent.appendChild(liElem);
     });
+}
+
+function renderCoincidences() {
+    let counter = getCoincidenceCounter(recommendedActivities, userFavoriteActivities, "Nombre");
+    let total = recommendedActivities.length;
+
+    coincidenceValueQuantitySpan.innerText = `${counter}/${total}`;
+    coincidenceValueAverageSpan.innerText = `${Math.round((counter / total) * 100)}%`
+}
+
+function getCoincidenceCounter(listA, listB, key) {
+    let coincidenceCounter = 0;
+    for (let i = 0; i < listA.length; i++) {
+        const elemA = listA[i];
+
+        for (let j = 0; j < listB.length; j++) {
+            const elemB = listB[j];
+            if(elemA[key] === elemB[key]) {
+                coincidenceCounter++;
+            }
+        }
+    }
+    return coincidenceCounter;
 }
